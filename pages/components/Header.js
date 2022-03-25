213196160b5cb36fb26d17175838ef1674357822
Leonardo Souza <leonardomibsp@hotmail.com>
2022-03-24 22:15:02 -0300

    fix indentation

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,10 +1,14 @@
-import { Center, Square, Circle, Img } from '@chakra-ui/react'
+import { Center } from '@chakra-ui/react'
 import { Image } from '@chakra-ui/react'
 
 export default function Header() {
     return (
-      <Center h='126px' boxShadow='md' mb='90px'>
-          <Image src='./Truephone.svg' />
+      <Center 
+        h='126px' 
+        boxShadow='md' 
+        mb='90px'>
+        <Image 
+        src='./Truephone.svg' />
       </Center>
     )
   }
--- pages/components/Upload.js
+++ pages/components/Upload.js
@@ -1,14 +1,13 @@
 import {Flex, Heading, Button, Container,Text, Link} from "@chakra-ui/react";
 import { Image } from '@chakra-ui/react'
-import Header from "./Header";
 
 export default function Upload() {
     return (
 <Container 
-  border='4px solid #3D3ADA' 
-  maxW={'842px'} h={'468px'} 
-  align='center' p={'85px'} 
-  borderRadius={'10px'} >
+  border='4px solid #3D3ADA'
+  maxW='842px' h='468px'
+  align='center' p='85px'
+  borderRadius='10px'>
 
   <Flex 
     direction='column' 
@@ -16,6 +15,7 @@ export default function Upload() {
     align='center' 
     margin-top= '90px'
     fontSize='md'>  
+    
     <Container>
       <Heading>
         <Image src='../Head.svg' />
@@ -31,9 +31,9 @@ export default function Upload() {
         Selecionar lista
        </Button>
 
-    <Container pt={'24px'}>
+    <Container pt='24px'>
       <Text 
-      fontWeight={'500'}
+      fontWeight='500'
       w='409px' 
       align='center'>
       Selecione um arquivo CSV para iniciar a verificação de uma lista com números e mensagens de SMS.
@@ -41,11 +41,11 @@ export default function Upload() {
     </Container>
       
     <Text 
-      fontWeight={'500'} 
-      color={'#747474'} 
-      pt={'24px'} 
-      fontSize={'xs'}>
-      Use nosso <Link href="#" color={'#3D3ADA'}><u>modelo</u></Link> de arquivo se você tem alguma dúvidas.
+      fontWeight='500'
+      color='#747474'
+      pt='24px'
+      fontSize='xs'>
+      Use nosso <Link href="#" color='#3D3ADA'><u>modelo</u></Link> de arquivo se você tem alguma dúvidas.
     </Text>
   </Flex>   
 </Container>       
